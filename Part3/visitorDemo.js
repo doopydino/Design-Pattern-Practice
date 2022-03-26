6837bb5cc507e43490e39ddbe36ca7d442c3da5c
Andrew Quinlan <andrewquinlan22@gmail.com>
2022-03-26 17:42:36 -0400

    part 3 step 2

--- Part3/visitorDemo.js
+++ Part3/visitorDemo.js
@@ -15,3 +15,27 @@ var MonsterTruckVisitor = function() {
         // Do some operations on monsterTruckVariable
     }
 }
+
+var carVariable = function() {
+    var seats = 5;
+    var doors = 4;
+    this.accept = function(visitorObject) {
+        visitorObject.visit(this);
+    }
+}
+
+var truckVariable = function() {
+    var towPackage = true;
+    var doors = 2;
+    this.accept = function(visitorObject) {
+        visitorObject.visit(this);
+    }
+}
+
+var monsterTruckVariable = function() {
+    var looksLikeADragon = true;
+    var doors = 1.5;
+    this.accept = function(visitorObject) {
+        visitorObject.visit(this);
+    }
+}
