946662d2726010db2a68601973495d1f7415afe9
Andrew Quinlan <andrewquinlan22@gmail.com>
2022-03-26 17:49:03 -0400

    part 3 step 3

--- Part3/visitorDemo.js
+++ Part3/visitorDemo.js
@@ -39,3 +39,31 @@ var monsterTruckVariable = function() {
         visitorObject.visit(this);
     }
 }
+
+var CarVisitor = function() {
+    this.visit = function(car) {
+        if(car.seats > 2) {
+            console.log('this is clearly a car for old people');
+        } else {
+            console.log('My bet is this car has at least 2 cylinders');
+        }
+    }
+}
+
+var TruckVisitor = function() {
+    this.visit = function(truckVar) {
+        if(truckVar.towPackage) {
+            console.log('we need to buy a boat');
+        }
+    }
+}
+
+var MonsterTruckVisitor = function() {
+    this.visit = function(monsterTruckVar) {
+        if(monsterTruckVar.looksLikeADragon) {
+            console.log('that is a badass monster truck');
+        } else {
+            console.log('loser');
+        }
+    }
+}
